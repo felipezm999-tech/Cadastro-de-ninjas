1deecf89ee105ec9e3eaa10428bdd68a0d8aed94
Felipe <felipezm999@gmail.com>
2026-09-16 14:00:55 -0300

    Criado a controller aula 02

--- src/main/java/dev/jav10x/CadastroDeNinja/CadastroDeNinjaApplication.java
+++ src/main/java/dev/jav10x/CadastroDeNinja/CadastroDeNinjaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CadastroDeNinjaApplication {
+public class 	CadastroDeNinjaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroDeNinjaApplication.class, args);
---
+++ src/main/java/dev/jav10x/CadastroDeNinja/controller.java
@@ -0,0 +1,18 @@
+package dev.jav10x.CadastroDeNinja;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class controller {
+
+
+    @GetMapping("/Boasvindas")
+    public String boasVindas() {
+        return "essa e minha primeira rota";
+    }
+
+
+}
