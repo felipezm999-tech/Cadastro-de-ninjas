65c4274d49fc9788066a2ef1a21ba936167874f9
Felipe <felipezm999@gmail.com>
2026-09-17 16:38:16 -0300

    atributos privados

--- src/main/java/dev/jav10x/CadastroDeNinja/NinjaModel.java
+++ src/main/java/dev/jav10x/CadastroDeNinja/NinjaModel.java
@@ -8,10 +8,10 @@ import jakarta.persistence.*;
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    long id ;
-    String nome;
-    String email;
-    int idade;
+    private long id;
+    private String nome;
+    private String email;
+    private int idade;
 
 
     public NinjaModel() {
